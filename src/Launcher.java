3e6b3658993b8ec22135570c6b8f400fe85ea3fb
Basma <basma.bouharicha@epita.fr>
2021-05-21 10:39:40 +0200

    partie3

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Launcher {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Bienvenue");
-        Scanner scanner= new Scanner(System.in);
-        String nxt= scanner.nextLine();
-        if(!nxt.equals("quit")) {
-            System.out.println("Unknown command");
-        }
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldQuit = false;
+        do {
+            System.out.println("Entrer une commande:");
+            String nxt = scanner.nextLine();
+            if (nxt.equals("quit")) {
+                shouldQuit = true;
+            } else {
+                System.out.println("Unknown command");
+            }
+        }while (!shouldQuit);
+
     }
+
+
+
 }
